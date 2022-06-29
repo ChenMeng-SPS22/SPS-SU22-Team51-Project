ff32e232721ffe4109444b0696a0d5741327b6b7
Renaud2002 <52769325+Renaud2002@users.noreply.github.com>
2022-06-28 21:20:47 -0400

    ListImages file change to sent json to js file and add async function to js file to take care of the json from ListImages

--- src/main/java/com/google/sps/servlets/ListImagesServlet.java
+++ src/main/java/com/google/sps/servlets/ListImagesServlet.java
@@ -5,7 +5,16 @@ import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.lang.reflect.Type;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,23 +33,34 @@ public class ListImagesServlet extends HttpServlet {
     Bucket bucket = storage.get(bucketName);
     Page<Blob> blobs = bucket.list();
 
-    // Output <img> elements as HTML. Would ideally be modified into json for later handling.
-    response.setContentType("text/html;");
-    for (Blob blob : blobs.iterateAll()) {
-      String imgTag = String.format("<img src=\"%s\" />", blob.getMediaLink());
-      response.getWriter().println(imgTag);
-      response.getWriter().println("<br>");
+
+    // List to store all the img html tag of the stored images
+    List<String> images_html = new ArrayList<String>();
+
+
+    // Adding cloud storage images as <img> html element to the list 
+    for(Blob blob : blobs.iterateAll()){
+        String imgTag = String.format("<img src=\"%s\" />", blob.getMediaLink());
+        images_html.add(imgTag);
     }
+
+    // converting the list to json using gson
+    String images_json = convertToJsonUsingGson(images_html);
+
+    // Output images as list of json <img> elements as HTML.
+    response.setContentType("application/json;");
+    response.getWriter().println(images_json);
+  }
+
+
+  // convert to json using gson
+  private String convertToJsonUsingGson(List<String> images_html) {
+
+    Type listType = new TypeToken<List<String>>() {}.getType();
+
+    Gson gson = new Gson();
+    String json = gson.toJson(images_html, listType);
+
+    return json;
   }
 }
-/*Idea for js function, would be in a diff .js file. You can ignore this if you want to do it another way .
-async function showImages(){
-    const responseFromServer = await fetch('/list-images');
-    const info = await responseFromServer.json();
-
-    const dashboardContainer = document.getElementById('dashboardContainer');
-    dashboardContainer.appendChild(Image1);
-    dashboardContainer.appendChild(Image2);
-    dashboardContainer.appendChild(Image..);
-}
-*/
