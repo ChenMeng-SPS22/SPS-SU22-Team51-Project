9f4994415b3658070eb1e0daa66f336facf29373
Renaud2002 <52769325+Renaud2002@users.noreply.github.com>
2022-07-14 00:04:53 -0400

    map sckeleton

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -98,3 +98,12 @@ async function image_upload() {
 }
 
 
+
+// Working on the map
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+
